42620b31e822e07d972c5c607fedfe0dc7b16d36
SpiderMan5269 <144431081+SpiderMan5269@users.noreply.github.com>
2023-09-20 00:36:00 +0530

    Update general.js

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -20,57 +20,97 @@ public_users.post("/register", (req,res) => {
 });
 
 // Get the book list available in the shop
-public_users.get('/',function (req, res) {
-     let myPromise = new Promise((resolve,reject) => {
-        setTimeout(() => {
-    res.send(JSON.stringify(books,null,4));
-        },6000)})
-    myPromise.then((successMessage) => {
-        console.log("From Callback " + successMessage)
-      })
+public_users.get('/', function (req, res) {
+  //Write your code here
+  getBooksAsync()
+    .then(response => {
+      return res.send(response);
+    }).catch(err => {
+      return res.status(404).json({ errorMessage: "Failed to get books" });
+    })
 });
+
+function getBooksAsync() {
+  return new Promise((resolve) => {
+    resolve(JSON.stringify(books, null, 4));
+  });
+}
 
 // Get book details based on ISBN
-public_users.get('/isbn/:isbn',function (req, res) {
-    let myPromise = new Promise((resolve,reject) => {
-        setTimeout(() => {
-            res.send(books[isbn])
-        },6000)})
-    const isbn = req.params.isbn;
-    myPromise.then((successMessage) => {
-        console.log("From Callback " + successMessage)
-      })
+public_users.get('/isbn/:isbn', function (req, res) {
+  const isbn = req.params.isbn;
+  getBookByIsbnAsync(isbn)
+    .then(bookRecord => {
+      return res.send(bookRecord);
+    }).catch(err => {
+      return res.status(404).json({ errorMessage: err });
+    })
 });
-  
+
+function getBookByIsbnAsync(isbn) {
+  return new Promise((resolve, reject) => {
+    if (books[isbn]) {
+      resolve(books[isbn]);
+    } else {
+      reject("Book Wasn't Found");
+    }
+  });
+}
+
 // Get book details based on author
-public_users.get('/author/:author',function (req, res) {
-    let myPromise = new Promise((resolve,reject) => {
-        setTimeout(() => {
-            res.send(books[author])
-        },6000)})
-        const author = req.params.author;
-        myPromise.then((successMessage) => {
-        console.log("From Callback " + successMessage)
-      })
+public_users.get('/author/:author', function (req, res) {
+
+  const author = req.params.author;
+
+  getBookByAuthorAsync(author)
+    .then(response => {
+      return res.send(response);
+    }).catch(err => {
+      return res.status(404).json({ errorMessage: err });
+    })
 });
+
+function getBookByAuthorAsync(author) {
+  return new Promise((resolve, reject) => {
+    const booksExist = Object.values(books).filter(book => book.author == author);
+    if (booksExist.length > 0) {
+      resolve(booksExist);
+    } else {
+      reject("Book Wasn't Found");
+    }
+  });
+}
 
 // Get all books based on title
-public_users.get('/title/:title',function (req, res) {
-    let myPromise = new Promise((resolve,reject) => {
-        setTimeout(() => {
-            res.send(books[title])
-        },6000)})
-        const title = req.params.title;
-        myPromise.then((successMessage) => {
-        console.log("From Callback " + successMessage)
-      })
+public_users.get('/title/:title', function (req, res) {
+
+  const title = req.params.title;
+
+  getBookByTitleAsync(title)
+    .then(response => {
+      return res.send(response);
+    }).catch(err => {
+      return res.status(404).json({ errorMessage: err });
+    })
+
 });
 
+function getBookByTitleAsync(title) {
+  return new Promise((resolve, reject) => {
+    const booksExist = Object.values(books).filter(book => book.title == title);
+    if (booksExist.length > 0) {
+      resolve(booksExist);
+    } else {
+      reject("Book Wasn't Found");
+    }
+  });
+}
+
 //  Get book review
-public_users.get('/review/:isbn',function (req, res) {
-    const isbn = req.params.isbn;
-    let book = books[isbn]
-    res.send(book["review"])
+public_users.get('/review/:isbn', function (req, res) {
+  const isbn = req.params.isbn;
+  const bookRecord = books[isbn];
+  return res.send(bookRecord ? bookRecord.reviews : "Book Wasn't Found");
 });
 
 module.exports.general = public_users;
